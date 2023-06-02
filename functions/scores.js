09eb6435d589b4c4715ccab97abdd708f97fdd2a
Caesura Media Net <andyc@caesuramedia.net>
2023-06-02 18:03:32 +0100

    No more than 5 scores kept.

--- functions/scores.js
+++ functions/scores.js
@@ -2,6 +2,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export async function addScore (score) {
   try {
 	let scores = await getScores ();
+	if (scores.length > 4) {
+		scores.shift();
+	}
 	scores.push (score);
     await AsyncStorage.setItem('@storage_Key', JSON.stringify(scores))
 	  return scores;
